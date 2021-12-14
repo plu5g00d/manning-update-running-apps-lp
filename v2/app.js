c7f0240de9609cc312068ad056fcb6e9e529881a
Sunil-Jacob <89004489+Sunil-Jacob@users.noreply.github.com>
2021-12-14 17:55:08 +0530

    version 2

---
+++ v2/app.js
@@ -0,0 +1,14 @@
+//Check if we are running in production environment or development env
+const http = require('http');
+const os = require('os');
+
+console.log("HomeJoy shopping app starting...");
+
+var handler = function(request, response) {
+  console.log("Received request from " + request.connection.remoteAddress);
+  response.writeHead(200);
+  response.end("This is version 2 running in pod " + os.hostname() + "\n");
+};
+
+var www = http.createServer(handler);
+www.listen(5000);
